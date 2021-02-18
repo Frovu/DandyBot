45fdc495b0a1f36ea2a11df2b25adace3ba7af9b
Frovu <sfrovis@gmail.com>
2021-02-18 20:16:08 +0300

    add: bots to gitignore

--- bots/user_bot.py
+++ bots/user_bot.py
@@ -1,25 +1,8 @@
 ﻿import random
+import random_bot
+
 
 def script(check, x, y):
-    def detectGold(x, y):
-        for i in range(100):
-            for ay in range(y-i, y+i+1):
-                if check("gold", x-i, ay): return x-i, ay
-            for ay in range(y-i, y+i+1):
-                if check("gold", x+i, ay): return x+i, ay
-            for ax in range(x-i, x+i+1):
-                if check("gold", ax, y+i): return ax, y+i
-            for ax in range(x-i, x+i+1):
-                if check("gold", ax, y-i): return ax, y-i
-        return -1, -1
-    print
-    if check("level") == 2 and x < 3 and y > 10:
-        return "up"
-    gx, gy = detectGold(x, y)
-    print((x,y), detectGold(x, y))
-    if gx < 0: return "pass"
-    if gx == x and gy == y: return "take"
-    if gx > x: return "right"
-    if gx < x: return "left"
-    if gy > y: return "down"
-    if gy < y: return "up"
+    if check("level") == 1:
+        return random_bot.script(check, x, y)
+    return "pass"
