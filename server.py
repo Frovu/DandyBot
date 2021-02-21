1bceb07be07e09e98bf8c6736572421b8b65c2c6
Frovu <sfrovis@gmail.com>
2021-02-21 15:18:06 +0300

    add: remote player connection

--- server.py
+++ server.py
@@ -5,33 +5,10 @@ from pathlib import Path
 from game import Game, Player
 import asyncio
 BOT_TILE = 2128
+PLAYER_TILE = 2138
 CHUNK = 128
 TICKRATE = 100
 CHALLENGES = Path('./game/challenges')
-
-class RemotePlayer(Player):
-    def __init__(self, game, player_name, bot_name, tile, reader, writer):
-        super().__init__(game, bot_name, tile)
-        self.username = player_name
-        self.reader = reader
-        self.writer = writer
-
-    def disconnect(self):
-        self.writer.close()
-
-class ServerGame(Game):
-    def __init__(self, challenge, tick_rate):
-        super().__init__(challenge)
-        self.tick_rate = tick_rate
-
-    async def connect_player(self, reader, writer):
-        writer.write("player".encode())
-        player_data = await reader.read(CHUNK)
-        try:
-            data = json.loads(player_data)
-        except:
-            writer.write("400".encode())
-            return await writer.drain()
 
 class Connection:
     def __init__(self, reader, writer):
@@ -44,16 +21,50 @@ class Connection:
             message += " " + json.dumps(object)
         self.writer.write(message.encode())
         await self.writer.drain()
-        resp = await reader.read(CHUNK)
+        resp = await  self.reader.read(CHUNK)
         resp = resp.decode()
         if not object is None and resp == "ok":
             return True
         try:
             return json.loads(resp)
         except:
-            writer.write("400".encode())
-            await writer.drain()
-            return False
+            await self.bad_request()
+            raise Exception("Failed to communicate: "+cmd)
+
+    async def bad_request(self):
+        self.writer.write("400".encode())
+        await self.writer.drain()
+
+    def close(self):
+        self.writer.close()
+
+class RemotePlayer(Player, Connection):
+    def __init__(self, game, reader, writer):
+        Connection.__init__(reader, writer)
+        self.username = None
+        self.game = game
+
+    async def connect(self):
+        data = await self.communicate("player")
+        username = data.get("name")
+        bot_name = data.get("bot")
+        bot_tile = data.get("tile")
+        if not self.username or not bot_name or not bot_tile:
+            await self.bad_request()
+            raise Exception("Bad player data")
+        self.username = str(username)
+        Player.__init__(self.game, str(bot_name), int(bot_tile))
+
+
+class ServerGame(Game):
+    def __init__(self, challenge, tick_rate):
+        super().__init__(challenge)
+        self.tick_rate = tick_rate
+
+    async def connect_player(self, reader, writer):
+        player = RemotePlayer(self, reader, writer)
+        await player.connect()
+        self.load_player(player)
 
 class Server:
     def __init__(self, ip, port):
@@ -65,12 +76,15 @@ class Server:
         chal_path = CHALLENGES.joinpath(title)
         return json.loads(chal_path.read_text())
 
-    async def start_game(self, reader, writer, chal_name):
+    async def start_game(self, chal_name, reader, writer):
         chal = self.load_challenge(chal_name)
         game = ServerGame(chal, TICKRATE)
-        self.games.append(game)
+        try:
+            await game.connect_player(reader, writer)
+            self.games.append(game)
+        except:
+            print("failed to connect player to solo game")
 
-        await game.connect_player(reader, writer)
 
     async def handler(self, reader, writer):
         data = await reader.read(CHUNK)
@@ -83,7 +97,7 @@ class Server:
         elif message.startswith("ping"):
             resp = "pong"
         elif message.startswith("start"):
-            await self.start_game("original.json")
+            await self.start_game("original.json", reader, writer)
             # TODO chose mode
             # start single player server challenge
 
